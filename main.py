d1a38b8c573e4d6648fa040ebcef319c482bc866
abyssalCodewright <zerosie@gmail.com>
2024-09-12 10:24:13 -0500

    player can rotate and move; added ignore to pygame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pygame
-import time
+import pygame # type: ignore
 from constants import *
 from player import Player
 
@@ -8,14 +7,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+
+    Player.containers = (updatable_group, drawable_group)
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for member in updatable_group:
+            member.update(dt)
         screen.fill('black')
-        player.draw(screen)
+        for member in drawable_group:
+            member.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
